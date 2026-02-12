951abb2b7b1bb985d046b496d9ba913a9d6a748b
Chenath Perera <chenathperera@gmail.com>
2026-02-12 23:10:19 +0530

    Admin setup

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ import { Server as SocketIOServer } from "socket.io";
 import { connectDB } from "./config/db.js";
 import { buildApp } from "./app.js";
 
+
 const PORT = Number(process.env.PORT || 5000);
 
 async function main() {

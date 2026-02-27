544d4ec350916b2756cd73f69ad7bd3260e4dd76
Tharushi <tharushi742@gmail.com>
2026-02-27 22:09:12 +0530

    Add routes for update,delete & getDriver reviews

--- server/src/routes/reviewRoutes.js
+++ server/src/routes/reviewRoutes.js
@@ -1,10 +1,18 @@
 import express from 'express';
-import { createReview, getDriverStats } from '../controllers/reviewController.js';
+import { 
+  createReview, 
+  updateReview, 
+  deleteReview, 
+  getDriverReviews // The aggregation function we built
+} from '../controllers/reviewController.js';
 // import { protect } from '../middleware/authMiddleware.js'; // Use your team's auth
 
 const router = express.Router();
 
 router.post('/submit', createReview); // Add 'protect' middleware here later
+router.get('/driver/:driverId', getDriverReviews);
+router.patch('/update/:id', updateReview);
+router.delete('/delete/:id', deleteReview);
 router.get('/stats/:driverId', getDriverStats);
 
 export default router;
